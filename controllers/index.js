24e4d63e009e60dbfdcb4e8dd6be4399399b72f6
Сорок два <Fortytwo@Fortytwo-HOME.localdomain>
2018-10-17 18:55:34 +0200

    fix: condition for result.private

--- controllers/index.js
+++ controllers/index.js
@@ -115,7 +115,7 @@ IndexController.postSaveScreen = (req, res) => {
 
 IndexController.getScreen = (req, res) => {
   Screen.getScreenshot(req.params.key, result => {
-    if (result.private) {
+    if (result.private === 1) {
       if (req.user && req.user.id === result.uploadBy) {
         Screen.increaseViews(req.params.key, result.views, callback => {
           res.render('screen', {src: result.path.replace('public', '')});
