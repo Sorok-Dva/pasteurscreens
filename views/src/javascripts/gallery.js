7f2b6868bf9a3e39b31508866eac1428784f90e7
root <root@lgarcia.tk>
2020-02-07 20:30:33 +0100

    fix setPrivacy

--- views/src/javascripts/gallery.js
+++ views/src/javascripts/gallery.js
@@ -49,7 +49,7 @@ const deleteScreen = (key) => {
 };
 
 const changePrivacy = (privacy, key) => {
-  const route = privacy === 1 || true ? 'public' : 'private';
+  const route = privacy === 1 || privacy === true ? 'public' : 'private';
   $.post(`/screens/set/${route}/${key}`, { _csrf }, result => {
     if (result.state === 'privacy updated') {
       if (route === 'public') {
